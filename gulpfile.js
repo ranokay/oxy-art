9aeb6fb4a7f143b3ac0c8eee82605939bc9fd770
Yuta <yuuta.dev@gmail.com>
2022-04-19 04:10:22 +0300

    3

--- gulpfile.js
+++ gulpfile.js
@@ -1,78 +1,109 @@
-const gulp = require('gulp'),
-	del = require('del'),
-	sass = require('gulp-sass')(require('sass')),
-	cleanCSS = require('gulp-clean-css'),
-	rename = require('gulp-rename'),
-	babel = require('gulp-babel'),
-	uglify = require('gulp-uglify'),
-	concat = require('gulp-concat'),
-	browserSync = require('browser-sync').create()
+import gulp from 'gulp'
+const { src, dest, watch, series, parallel } = gulp
+import del from 'del'
+import dartSass from 'sass'
+import gulpSass from 'gulp-sass'
+const sass = gulpSass(dartSass)
+import cleanCSS from 'gulp-clean-css'
+import rename from 'gulp-rename'
+import babel from 'gulp-babel'
+import uglify from 'gulp-uglify'
+import concat from 'gulp-concat'
+import sourcemaps from 'gulp-sourcemaps'
+const { init, write } = sourcemaps
+import autoprefixer from 'gulp-autoprefixer'
+import imagemin from 'gulp-imagemin'
+import htmlmin from 'gulp-htmlmin'
+import newer from 'gulp-newer'
+import browserSync from 'browser-sync'
 
 const paths = {
+	html: {
+		src: 'src/**/*.html',
+		dest: ['dist'],
+	},
 	styles: {
-		src: 'src/sass/**/*.sass',
+		src: ['src/sass/**/*.sass', 'src/sass/**/*.scss'],
 		dest: 'dist/css',
 	},
 	scripts: {
 		src: 'src/js/**/*.js',
 		dest: 'dist/js',
 	},
+	images: {
+		src: 'src/images/**/*',
+		dest: 'dist/images',
+	},
 }
 
 //clean task
 function cleanTask() {
-	return del(['dist'])
+	return del(['dist/*', '!dist/images'])
 }
 
-//sass task
-function sassTask() {
-	return gulp
-		.src(paths.styles.src)
-		.pipe(sass())
-		.pipe(cleanCSS())
+//htmlmin task
+function htmlminTask() {
+	return src(paths.html.src)
+		.pipe(htmlmin({ collapseWhitespace: true }))
+		.pipe(dest(paths.html.dest))
+		.pipe(browserSync.stream())
+}
+
+//style task
+function styleTask() {
+	return src(paths.styles.src)
+		.pipe(init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(
+			autoprefixer({
+				cascade: false,
+			})
+		)
+		.pipe(
+			cleanCSS({
+				level: 2,
+			})
+		)
 		.pipe(
 			rename({
-				basename: 'main',
+				basename: 'style',
 				suffix: '.min',
 			})
 		)
-		.pipe(gulp.dest(paths.styles.dest))
+		.pipe(write('.'))
+		.pipe(dest(paths.styles.dest))
+		.pipe(browserSync.stream())
 }
 
 //js task
 function jsTask() {
-	return gulp
-		.src(paths.scripts.src, { sourcemaps: true })
-		.pipe(babel())
+	return src(paths.scripts.src)
+		.pipe(init())
+		.pipe(
+			babel({
+				presets: ['@babel/preset-env'],
+			})
+		)
 		.pipe(uglify())
 		.pipe(concat('main.min.js'))
-		.pipe(gulp.dest(paths.scripts.dest))
+		.pipe(write('.'))
+		.pipe(dest(paths.scripts.dest))
+		.pipe(browserSync.stream())
 }
 
-//browserSync serve
-function browserSyncServe(cb) {
-	browserSync.init({
-		proxy: 'http://localhost/Oxy-Project/src',
-	})
-	cb()
+//imagemin task
+function imgminTask() {
+	return src(paths.images.src)
+		.pipe(newer(paths.images.dest))
+		.pipe(
+			imagemin({
+				quality: 80,
+				progressive: true,
+				optimizationLevel: 2,
+			})
+		)
+		.pipe(dest(paths.images.dest))
 }
-//browserSync reload
-function browserSyncReload(cb) {
-	browserSync.reload()
-	cb()
-}
-
-//Images
-// function imagesCompress() {
-// 	return src('src/images/*.{jpg,jpeg,png,gif}')
-// 		.pipe(
-// 			imagemin([
-// 				imagemin.mozjpeg({ quality: 80, progressive: true }),
-// 				imagemin.optipng({ optimizationLevel: 2 }),
-// 			])
-// 		)
-// 		.pipe(dest('dist/images'))
-// }
 
 //Webp images
 // function imagesWebp() {
@@ -81,18 +112,27 @@ function browserSyncReload(cb) {
 
 //watch task
 function watchTask() {
-	gulp.watch(paths.styles.src, sassTask)
-	gulp.watch(paths.scripts.src, jsTask)
+	browserSync.init({
+		proxy: {
+			target: 'http://localhost/Oxy-Project/dist/',
+		},
+		tunnel: true,
+	})
+	watch(paths.html.dest).on('change', browserSync.reload)
+	watch(paths.html.src, htmlminTask)
+	watch(paths.styles.src, styleTask)
+	watch(paths.scripts.src, jsTask)
+	watch(paths.images.src, imgminTask)
 }
 
-//build task
-const build = gulp.series(cleanTask, gulp.parallel(sassTask, jsTask), watchTask)
+//gulp tasks
+export const _clean = cleanTask
+export const _htmlmin = htmlminTask
+export const _style = styleTask
+export const _js = jsTask
+export const _imgmin = imgminTask
+export const _watch = watchTask
 
 //default gulp task
-exports.default = build
-exports.build = build
-
-exports.cleanTask = cleanTask
-exports.sassTask = sassTask
-exports.jsTask = jsTask
-exports.watchTask = watchTask
+const _default = series(cleanTask, htmlminTask, parallel(styleTask, jsTask, imgminTask), watchTask)
+export { _default as default }
